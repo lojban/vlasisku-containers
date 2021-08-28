5dfbdec893fdabfa25b0f97a1aee7c070565b243
Robin Lee Powell <robinleepowell@gmail.com>
2021-08-28 10:00:55 -0700

    Make run_bots actually run at the right time.

--- misc/run_bots.sh
+++ misc/run_bots.sh
@@ -11,6 +11,28 @@ lbcsdir="/opt/lbcs"
 . $lbcsdir/config
 . $maindir/config
 
+# Stolen from run_container.sh
+for after_container in web
+do
+    for num in $(seq 1 10)
+    do
+        if [[ $($CONTAINER_BIN container inspect --format '{{.State.Status}}' $after_container) == 'running' ]]
+        then
+            break
+        fi
+        echo -e "\nWaiting for required container $after_container to start.\n"
+        sleep 30
+    done
+
+    if [[ $($CONTAINER_BIN container inspect --format '{{.State.Status}}' $after_container) == 'running' ]]
+    then
+        echo -e "\nRequired container $after_container has started.\n"
+    else
+        echo -e "\nRequired container $after_container has not started; exiting.\n"
+        exit 1
+    fi
+done
+
 echo
 echo "Launching container bots inside the previous container instance"
 echo
